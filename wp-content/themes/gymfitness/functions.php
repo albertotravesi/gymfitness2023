4faaf374058e69d73e4d3cb306bc06f3dccb0d76
Alberto Travesi <albertotravesi7@gmail.com>
2022-12-20 14:26:51 -0600

    Normalize, Google Fonts, Estilos globales y utilidades de css

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -6,6 +6,14 @@ function gymfitness_menus() {
     ));
 }
 
-add_action('init', 'gymfitness_menus')
+add_action('init', 'gymfitness_menus');
+
+
+function gymfitness_scripts_styles(){
+    
+    wp_enqueue_style('normalize', 'https://necolas.github.io/normalize.css/8.0.1/normalize.css', array(), '8.0.1');
+    wp_enqueue_style('style', get_stylesheet_uri(), array('normalize'), '1.0.0');
+}
+add_action( 'wp_enqueue_scripts', 'gymfitness_scripts_styles');
 
 ?>
